a811c69e5ac640bd3621387ef48444bc66db0823
Alice <>
2023-01-29 10:21:06 +0800

    refactor RWD

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,44 @@
 <template>
   <div class="container">
     <div class="navs">
-      <div class="hamburger">
-        <div class="line line1"></div>
-        <div class="line line2"></div>
-        <div class="line line3"></div>
-      </div>
-      <div class="box">
-        <div class="items">
-          <div>Çözüm ve Hizmetler</div>
-          <div>Ürünler</div>
-          <div>Teknolojiler</div>
-          <div>İnsan Kaynakları</div>
-          <div>Kurumsal</div>
+      <div class="mobile">
+        <div class="hamburger">
+          <div class="line line1"></div>
+          <div class="line line2"></div>
+          <div class="line line3"></div>
         </div>
-        <div class="redbutton">İletişim</div>
       </div>
+      <div class="desktop">
+        <div class="box">
+          <div class="items">
+            <div>Çözüm ve Hizmetler</div>
+            <div>Ürünler</div>
+            <div>Teknolojiler</div>
+            <div>İnsan Kaynakları</div>
+            <div>Kurumsal</div>
+          </div>
+        </div>
+      </div>
+      <div class="redbutton">İletişim</div>
     </div>
 
-    <div class="contents">
+    <div class="contents mobile">
       <div class="title">Bilgi <br />Teknolojilerinde <br />23 Yıllık Tecrübe</div>
       <div class="content">
         Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman<br />
         kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
       </div>
-      <div class="mail">
-        <div>
-          <input type="text" placeholder="Mail bültenimize kayıt ol" />
-        </div>
-        <div class="redbutton mailsend">Kayıt Ol</div>
-      </div>
     </div>
-    <div class="contents1">
-      <div class="title1">Bilgi Teknolojilerinde <br />23 Yıllık Tecrübe</div>
-      <div class="content1">
+    <div class="contents desktop">
+      <div class="title">Bilgi Teknolojilerinde <br />23 Yıllık Tecrübe</div>
+      <div class="content">
         Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı <br />sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş<br />
         Analizi ve Test Yönetimi hizmetleri sunmaktayız.
       </div>
-      <div class="mail1">
+    </div>
+
+    <div>
+      <div class="mail">
         <div>
           <input type="text" placeholder="Mail bültenimize kayıt ol" />
         </div>
@@ -53,6 +54,7 @@
 .container {
   background-image: radial-gradient(circle at 141% 78%, #fff, rgba(255, 255, 255, 0) 107%), url("@/assets/images/rectangle-1.jpg");
   background-size: cover;
+  height: 100vh;
   background-position: right 35% bottom 45%;
   width: 100%;
 }
@@ -72,9 +74,18 @@
   width: 91px;
   text-align: center;
 }
+.items > div:hover {
+  cursor: pointer;
+  color: #0f29e8;
+}
+.redbutton:hover {
+  cursor: pointer;
+  background-color: #0f29e8;
+  transition: 0.8s;
+}
 .contents {
-  padding: 20px;
   text-align: center;
+  width: 100%;
 }
 .title {
   font-size: 40px;
@@ -90,6 +101,7 @@
   justify-content: space-between;
   padding: 10px;
   align-items: center;
+  margin: 10px;
 }
 .mail input {
   outline: none;
@@ -118,22 +130,30 @@
 }
 
 @media (min-width: 375px) and (max-width: 1023px) {
+  .mobile {
+    display: block !important;
+  }
+  .desktop {
+    display: none !important;
+  }
   .items {
     display: none !important;
   }
   .hamburger {
     visibility: visible;
   }
-  .contents1 {
-    display: none !important;
-  }
 }
 @media (min-width: 1024px) and (max-width: 1439px) {
+  .mobile {
+    display: none !important;
+  }
+  .desktop {
+    display: block !important;
+  }
   .container {
     background-image: url("@/assets/images/rectangle-1.jpg");
-    background-size: cover;
+    height: 100vh;
     background-position: 0px 0px;
-    width: 100%;
   }
   .navs {
     display: flex;
@@ -142,9 +162,9 @@
     display: none !important;
   }
   .navs {
-    display: flex;
     justify-content: flex-end;
   }
+
   .box {
     display: flex;
     margin: 30px;
@@ -156,42 +176,31 @@
     display: flex;
     align-items: center;
   }
-  .items > div:hover {
-    cursor: pointer;
-    color: #0f29e8;
-  }
-  .redbutton:hover {
-    cursor: pointer;
-    background-color: #0f29e8;
-    transition: 0.8s;
-  }
-  .contents {
-    display: none !important;
-  }
-  .contents1 {
-    width: 665px;
-    padding: 50px;
-    margin: 20px;
-    margin-left: 122px;
-  }
-  .title1 {
+
+  .title {
     font-size: 62px;
+    text-align: left;
+    margin-left: 122px;
+    margin-top: 93px;
   }
-  .content1 {
+  .content {
     margin-top: 20px;
     line-height: 1.8;
     font-size: 19px;
+    text-align: left;
+    margin-left: 122px;
   }
-  .mail1 {
+  .mail {
     display: flex;
     background: #fff;
     width: 502px;
     border-radius: 8px;
     justify-content: space-between;
     padding: 6px 6px 6px 32px;
-    margin: 50px 163px 0 0;
+    margin: 30px 163px 0 0;
+    margin-left: 122px;
   }
-  .mail1 input {
+  .mail input {
     border: none;
     outline: none;
     align-items: center;
@@ -199,26 +208,29 @@
   }
 }
 @media (min-width: 1440px) {
+  .mobile {
+    display: none !important;
+  }
+  .desktop {
+    display: block !important;
+  }
   .container {
     background-image: url("@/assets/images/rectangle-1.jpg");
-    background-repeat: cover;
+
     background-position: 0px 0px;
-    width: 100%;
   }
-  .navs {
-    display: flex;
-  }
+
   .hamburger {
     display: none !important;
   }
   .navs {
-    display: flex;
     justify-content: flex-end;
   }
+
   .box {
     display: flex;
     margin: 50px;
-    margin-right: 40px;
+    margin-right: 0px;
   }
   .box div {
     margin-right: 25px;
@@ -227,45 +239,27 @@
     display: flex;
     align-items: center;
   }
-  .items > div:hover {
-    cursor: pointer;
-    color: #0f29e8;
-  }
-  .redbutton:hover {
-    cursor: pointer;
-    background-color: #0f29e8;
-    transition: 0.8s;
-  }
-  .contents {
-    display: none !important;
-  }
-  .contents1 {
-    width: 665px;
-    padding: 50px;
-    margin: 20px;
-    margin-left: 122px;
-  }
-  .title1 {
+
+  .title {
     font-size: 62px;
+    text-align: left;
+    margin-left: 122px;
+    margin-top: 93px;
   }
-  .content1 {
+  .content {
     margin-top: 20px;
     line-height: 1.8;
     font-size: 19px;
+    text-align: left;
+    margin-left: 122px;
   }
-  .mail1 {
-    display: flex;
-    background: #fff;
+  .mail {
     width: 502px;
-    border-radius: 8px;
-    justify-content: space-between;
     padding: 6px 6px 6px 32px;
-    margin: 50px 163px 0 0;
+    margin: 30px 163px 0px 0;
+    margin-left: 122px;
   }
-  .mail1 input {
-    border: none;
-    outline: none;
-    align-items: center;
+  .mail input {
     margin: 13px 150px 12px 0;
   }
 }
