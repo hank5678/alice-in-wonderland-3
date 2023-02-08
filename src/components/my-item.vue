5abf169202159d60b5d4a0bd8c745b230e028a61
s0901598 <s0901598@gmail.com>
2023-02-08 22:58:43 +0800

    多個slot

--- src/components/my-item.vue
+++ src/components/my-item.vue
@@ -1,56 +1,54 @@
 <template>
-  <div class="item" v-for="item of items" :key="item.id">
-    <div class="item__icon">
-      <img :src="item.icon" alt="" />
-    </div>
-    <div class="item__title">
-      {{ item.title }}
-    </div>
-    <div class="item__content">
-      {{ item.content }}
-    </div>
+  <div>
+    <slot name="item__icon"></slot>
+  </div>
+  <div>
+    <slot name="item__title"></slot>
+  </div>
+  <div>
+    <slot name="item__content"></slot>
   </div>
 </template>
 <script setup>
-import { ref } from "vue"
-import icon1 from "@/assets/icons/group 27.png"
-import icon2 from "@/assets/icons/group 29.png"
-import icon3 from "@/assets/icons/group 32.png"
-import icon4 from "@/assets/icons/group 31.png"
-import icon5 from "@/assets/icons/group 33.png"
-import icon6 from "@/assets/icons/group 30.png"
-let items = ref([
-  {
-    icon: icon1,
-    title: "Döküman Analizi",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  },
-  {
-    icon: icon2,
-    title: "Kabul ve Değerlendirme",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  },
-  {
-    icon: icon3,
-    title: "Kabul ve Değerlendirme",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  },
-  {
-    icon: icon4,
-    title: "Kabul ve Değerlendirme",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  },
-  {
-    icon: icon5,
-    title: "Kabul ve Değerlendirme",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  },
-  {
-    icon: icon6,
-    title: "Kabul ve Değerlendirme",
-    content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
-  }
-])
+// import { ref } from "vue"
+// import icon1 from "@/assets/icons/group 27.png"
+// import icon2 from "@/assets/icons/group 29.png"
+// import icon3 from "@/assets/icons/group 32.png"
+// import icon4 from "@/assets/icons/group 31.png"
+// import icon5 from "@/assets/icons/group 33.png"
+// import icon6 from "@/assets/icons/group 30.png"
+// let items = ref([
+//   {
+//     icon: icon1,
+//     title: "Döküman Analizi",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   },
+//   {
+//     icon: icon2,
+//     title: "Kabul ve Değerlendirme",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   },
+//   {
+//     icon: icon3,
+//     title: "Kabul ve Değerlendirme",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   },
+//   {
+//     icon: icon4,
+//     title: "Kabul ve Değerlendirme",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   },
+//   {
+//     icon: icon5,
+//     title: "Kabul ve Değerlendirme",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   },
+//   {
+//     icon: icon6,
+//     title: "Kabul ve Değerlendirme",
+//     content: "Ante vulputate ut at accumsan et. Feugiat at tempus, est senectus amet, elementum."
+//   }
+// ])
 </script>
 <style scoped>
 .item__icon {
@@ -85,11 +83,13 @@ let items = ref([
 @media (min-width: 1024px) and (max-width: 1439px) {
   .item {
     width: 50%;
+    text-align: center;
   }
 }
 @media (min-width: 1440px) {
   .item {
     width: 33%;
+    text-align: center;
   }
 }
 </style>
